0852bb1a53526540c8429944cb22b84361a0b435
Talar25 <jjasiota@gmail.com>
2023-11-24 17:05:54 +0100

    add directory setup to the repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<h1>Homepage</h1>}></Route>
+      <Route path='/women' element={<h1>Women</h1>}>
+        <Route path='/women-tshirts' element={<h1>Women tshirts</h1>}></Route>
+        <Route path='/women-jackets' element={<h1>Women jackets</h1>}></Route>
+        <Route
+          path='/women-sweatshirts'
+          element={<h1>Women sweatshirts</h1>}
+        ></Route>
+        <Route path='/women-trousers' element={<h1>Women trousers</h1>}></Route>
+      </Route>
+      <Route path='/men' element={<h1>Men</h1>}>
+        <Route path='men-tshirts' element={<h1>Men tshirts</h1>}></Route>
+        <Route path='men-jackets' element={<h1>Men jackets</h1>}></Route>
+        <Route
+          path='men-sweatshirts'
+          element={<h1>Men sweatshirts</h1>}
+        ></Route>
+        <Route path='men-trousers' element={<h1>Men trousers</h1>}></Route>
+      </Route>
+      <Route path='/children' element={<h1>Children</h1>}>
+        <Route
+          path='/children-tshirts'
+          element={<h1>Children tshirts</h1>}
+        ></Route>
+        <Route
+          path='/children-jackets'
+          element={<h1>Children jackets</h1>}
+        ></Route>
+        <Route
+          path='/children-sweatshirts'
+          element={<h1>Children sweatshirts</h1>}
+        ></Route>
+        <Route
+          path='/children-trousers'
+          element={<h1>Children trousers</h1>}
+        ></Route>
+      </Route>
+      <Route path='/cart' element={<h1>Shopping cart</h1>}></Route>
+      <Route path='*' element={<h1>page not found</h1>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
+
